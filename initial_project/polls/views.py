14826e30c94c2266eda0059f7bf40c4abce0a637
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-16 11:20:02 +0500

    feat: added simple views and learnt views and their urls.

--- initial_project/polls/views.py
+++ initial_project/polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world! you're at the polls application's index. ")
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question: {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
+
