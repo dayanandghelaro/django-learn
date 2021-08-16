86d59cf660938a052edaee2c2b1cbb7a06c4209f
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-16 14:49:13 +0500

    feat: added Http404 exception and other template files.

--- initial_project/polls/views.py
+++ initial_project/polls/views.py
@@ -1,25 +1,52 @@
-from django.shortcuts import render
-from django.template import loader
-
-from django.http import HttpResponse
+"""This module contains views."""
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Question
 
+
 # Create your views here.
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:]
-    template = loader.get_template("polls/index.html")
+    """Show default homepage."""
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question: {question_id}")
+    """Detail of question."""
+    try:
+        question = Question.objects.get(pk=question_id)
+        options = question.choice_set.all()
+        context = {
+            'question': question,
+            "options": options,
+        }
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!")
+    return render(request, "polls/detail.html", context)
+
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}")
+    """Show result of votes."""
+    question = get_object_or_404(Question, question_id)
+    context = {
+        "question": question,
+    }
+    return render(request, "polls/results.html", context)
+
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    """Vote any question."""
+    return render(request, "polls/vote.html")
+
+
+
+
+
+
+
+
 
