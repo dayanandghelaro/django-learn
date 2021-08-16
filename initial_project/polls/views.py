fcc447a67028d1ef16f165756586548485691bd1
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-16 11:38:26 +0500

    feat: added index view to show list of questions available

--- initial_project/polls/views.py
+++ initial_project/polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world! you're at the polls application's index. ")
+    question_list = Question.objects.order_by('-pub_date')[:]
+    output = ", ".join([q.question_text for q in question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse(f"You're looking at question: {question_id}")
