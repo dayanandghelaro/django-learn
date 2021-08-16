0b105934a9917ac60921fcbcf481bca219f0a457
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-16 12:19:46 +0500

    feat: add templates and load using django.templates.loader

--- initial_project/polls/views.py
+++ initial_project/polls/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.template import loader
+
 from django.http import HttpResponse
 
 from .models import Question
 
 # Create your views here.
 def index(request):
-    question_list = Question.objects.order_by('-pub_date')[:]
-    output = ", ".join([q.question_text for q in question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse(f"You're looking at question: {question_id}")
