87b9839ad3589da1092eb4b6e775cb8c54e4e8cd
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-17 13:07:51 +0500

    feat: use generic views and update urls

--- initial_project/initial_project/urls.py
+++ initial_project/initial_project/urls.py
@@ -1,4 +1,4 @@
-"""initial_project URL Configuration
+"""Initial_project URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include("polls.urls")),
     path('admin/', admin.site.urls),
-    # path('polls/', include("polls.urls")),
 ]
--- initial_project/polls/views.py
+++ initial_project/polls/views.py
@@ -1,31 +1,34 @@
 """This module contains views."""
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.views import generic
 
 from .models import Question, Choice
 
 
 # Create your views here.
-def index(request):
-    """Show default homepage."""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    return render(request, "polls/index.html", {
-        "latest_question_list": latest_question_list
-    })
+class IndexView(generic.ListView):
+    """Index view of polls."""
 
+    template_name = 'polls/index.html'
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    """Detail of question."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
+class DetailView(generic.DetailView):
+    """Detail view of polls."""
 
-def results(request, question_id):
-    """Show result of votes."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {'question': question})
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    """Results view of polls."""
+
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
