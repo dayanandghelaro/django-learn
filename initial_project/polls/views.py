553ded0af089c2753ec7ba523e8a65cbf8c2f781
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-17 10:14:00 +0500

    feat: add vote and result section.

--- initial_project/polls/views.py
+++ initial_project/polls/views.py
@@ -1,8 +1,10 @@
 """This module contains views."""
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+
+from .models import Question, Choice
 
 
 # Create your views here.
@@ -19,10 +21,8 @@ def detail(request, question_id):
     """Detail of question."""
     try:
         question = Question.objects.get(pk=question_id)
-        options = question.choice_set.all()
         context = {
             'question': question,
-            "options": options,
         }
     except Question.DoesNotExist:
         raise Http404("Question does not exist!")
@@ -31,7 +31,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     """Show result of votes."""
-    question = get_object_or_404(Question, question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {
         "question": question,
     }
@@ -40,13 +40,15 @@ def results(request, question_id):
 
 def vote(request, question_id):
     """Vote any question."""
-    return render(request, "polls/vote.html")
-
-
-
-
-
-
-
-
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice!"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
