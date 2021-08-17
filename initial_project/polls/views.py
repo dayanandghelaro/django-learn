19bf4e1e4d998afb8cfde84d86eb4e91f761779b
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-17 14:24:49 +0500

    fix: remove race condition

--- initial_project/polls/views.py
+++ initial_project/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.db.models import F
 
 from .models import Question, Choice
 
@@ -42,6 +43,6 @@ def vote(request, question_id):
             "error_message": "You didn't select a choice!"
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
