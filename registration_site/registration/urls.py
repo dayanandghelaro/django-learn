6c016b4a5c7573e239bd996f7b662fdfb1bb3f1b
DayanandGhelaro <dayanandghelaro@A006-00592.local>
2021-08-23 11:31:41 +0500

    feat: add index, login, signup, logout, and home.

--- registration_site/registration/urls.py
+++ registration_site/registration/urls.py
@@ -1,6 +1,14 @@
 """Urls of registration app."""
+from django.urls import path
 
+from registration import views
 
+app_name = 'registration'
 urlpatterns = [
-    
+    path("", views.index, name='index'),
+    path("signup/", views.signup, name='signup'),
+    path("login/", views.login, name='login'),
+    path("logout/", views.logout_user, name="logout"),
+    path("home/<str:username>", views.home, name='home'),
+
 ]
